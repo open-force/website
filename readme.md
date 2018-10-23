c57113d71c185b68a1611f874b2fb7c44e633028
Charles Jonas <charlie@callawaycloudconsulting.com>
2018-10-23 13:33:38 -0600

    - Refactored to use SFDC backend instead of json file - Added website as submodule

--- readme.md
+++ readme.md
@@ -3,25 +3,23 @@
 Proof of concept
 
 ## Adding new Repositories
-- Submit a PR updating [/data/repositories.json](https://github.com/open-force/website/blob/master/data/repositories.json) with the repo's you'd like to add 
-- site will be updated when PR is merged into master
 
 ## Setup
-
 
 ### install
 
 Project has 2 package.json files, so you will need to run `npm install` twice.
 
-1. `git clone`
+1. `git clone --recursive [URL]`: includes the website as a submodule
 2. `cd`
 3. `npm install`
-4. `cd react-app`
-5. `npm install`
+4. `npm run new-org`: Creates a scratch org and deploys code in `./website-org` submodule. If you already have an org you want to use, cd into `website-org` and run `sfdx force:config:set defaultusername={ORG}`.
+5. `cd react-app`
+6. `npm install`
 
 ### .env
 
-Project is configured with [dotenv](https://www.npmjs.com/package/dotenv) to load enviormental vars
+Project is configured with [dotenv](https://www.npmjs.com/package/dotenv) to load environmental vars
 
 1. Create a `.env` file in root of project
 2. add values for the following:
@@ -31,15 +29,6 @@ GITHUB_USER=
 GITHUB_PASS=
 ```
 
-## Deploy to heroku
-
-1. `heroku create`
-1. `git push heroku master`
-1. use the `heroku` cli or user interface to setup "config vars" for same `.env` properties above
-
-Note: Uses the `heroku-postbuild` script to build the react app upon deployment.  This results in a slightly slower deploy time.
-
-
 ## Development
 
 1. open terminal
@@ -47,5 +36,14 @@ Note: Uses the `heroku-postbuild` script to build the react app upon deployment.
 1. open new tab
 1. `npm run dev-client`
 
-- Uses `nodemon` to automaticly update the server on changes.
+- Uses `nodemon` to automatically update the server on changes.
 - Uses `webpack-dev-server` to hot reload client changes.
+- When `NODE_ENV`is not production, the server will connect using default user found in `./website-org/.sfdx/sfdx-config.json`
+
+## Deploy to heroku
+
+1. `heroku create`
+1. `git push heroku master`
+1. use the `heroku` cli or user interface to setup "config vars" for same `.env` properties above
+
+Note: Uses the `heroku-postbuild` script to build the react app upon deployment.  This results in a slightly slower deploy time.
--- server/index.ts
+++ server/index.ts
@@ -1,60 +1,35 @@
 import * as express from 'express';
 import * as path from 'path';
-import { Github } from './lib/githubClient';
-import * as fs from 'fs';
-import * as cache from 'memory-cache'
+import sfdcAuth from './lib/sfdcAuthService'
+import { ResourceService } from './lib/resourceService';
 
-require('dotenv').config();
+require('dotenv').config({ encoding: 'utf8' });
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 
-const github = new Github({
-  username: process.env.GITHUB_USER,
-  password: process.env.GITHUB_PASS
-});
+sfdcAuth();
+start();
 
-const repoData: AppData = JSON.parse(
-  fs.readFileSync(path.resolve(__dirname, '../data/repositories.json')).toString()
-);
+async function start() {
 
-const app = express();
-const PORT = process.env.PORT || 5000;
+  const app = express();
+  const PORT = process.env.PORT || 5000;
 
-// Priority serve any static files.
-app.use(express.static(path.resolve(__dirname, '../react-app/dist')));
+  let resourceService = new ResourceService();
 
-app.get('/api/repos', async function (request, response) {
-  response.send(await getRepoData());
-});
+  // Priority serve any static files.
+  app.use(express.static(path.resolve(__dirname, '../react-app/dist')));
 
-// All remaining requests return the React app, so it can handle routing.
-app.get('*', function (request, response) {
-  response.sendFile(path.resolve(__dirname, '../react-app/dist', 'index.html'));
-});
+  app.get('/api/repos', async function (request, response) {
+    response.send(await resourceService.getRepoData());
+  });
 
-app.listen(PORT, function () {
-  console.error(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
-});
+  // All remaining requests return the React app, so it can handle routing.
+  app.get('*', function (request, response) {
+    response.sendFile(path.resolve(__dirname, '../react-app/dist', 'index.html'));
+  });
 
-const CACHE_KEY = 'repos'
-const CACHE_TIMEOUT = 60*5*1000;
-async function getRepoData(){
-  //check cache
-  if(cache.get(CACHE_KEY)){
-    return cache.get(CACHE_KEY);
-  }
-  console.log('no cache found!')
+  app.listen(PORT, function () {
+    console.error(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
+  });
 
-  //pull data
-  let data: Repository[] = [];
-  for (let item of repoData) {
-    for (let repo of item.repositories) {
-      try{
-        let response = await github.getRepo(item.user, repo);
-        data.push(response.data);
-      }catch(e){
-        console.log(e);
-      }
-    }
-  }
-  cache.put(CACHE_KEY, data, CACHE_TIMEOUT);
-  return data;
 }
