c7f620723282cfaaf57673b6a5508d0967427e78
Charles Jonas <charlie@callawaycloudconsulting.com>
2018-10-24 20:20:43 -0600

    added import for styles

--- website/react-app/src/index.tsx
+++ website/react-app/src/index.tsx
@@ -5,6 +5,7 @@ import App from './app';
 
 // add react styles
 // tslint:disable-next-line:no-var-requires
+require('./styles/styles.less');
 require('@salesforce-ux/design-system/assets/styles/salesforce-lightning-design-system.min.css');
 
 ReactDOM.render(<App />, document.getElementById('root') as HTMLElement);
